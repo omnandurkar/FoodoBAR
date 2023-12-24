3c98ce8e03ef9d0a3ba117da316f489c13b09e28
Sneha-Devare <Snehadevare158@gmail.com>
2023-12-24 16:33:42 +0530

    new commit

---
+++ javascript/menu.js
@@ -0,0 +1,9 @@
+let tracking = document.getElementById("tracking-btn");
+tracking.addEventListener("click", () => {
+   alert("Your Order Is Tracking Now ");
+});
+let newbtn = document.getElementById("red-btn-js");
+newbtn.addEventListener("mouseover", () => {
+   alert("Your Order Is Tracking Now ");
+});
+ge
